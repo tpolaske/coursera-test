ca7fe5ad1356cd5e7a4e63b7ac3f2b3b9f381c13
tpolaske <tpolaske@gmail.com>
2017-03-09 17:07:21 -0500

    fixed catshortname

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = JSON.stringify(chooseRandomCategory(categories));
+      var chosenCategoryShortName = JSON.stringify(chooseRandomCategory(categories).short_name);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,9 +115,10 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl,
                                                         "randomCategoryShortName",  
-                                                        chosenCategoryShortName)
+                                                        chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
